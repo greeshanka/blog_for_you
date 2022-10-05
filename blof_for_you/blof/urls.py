8806c34cd6653d16d52dc7ea3d1489715d062dee
Alex Malyshex <greeshanka@gmail.com>
2022-10-05 18:50:38 +0300

    realization templates sample and creation base templates

--- blof_for_you/blof/urls.py
+++ blof_for_you/blof/urls.py
@@ -3,6 +3,6 @@ from blof.views import index, list_category
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='home'),
     path('category/<int:category_id>/', list_category, name='list_category'),
 ]
