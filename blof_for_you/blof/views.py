c880dd95041d5016879c7db349bf2737b08c0a34
Alex Malyshex <greeshanka@gmail.com>
2022-10-05 21:23:20 +0300

    added new_tags.html; changed all templates, because there are so many repetitions

--- blof_for_you/blof/views.py
+++ blof_for_you/blof/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Blog, Category
 
 
 def index(request):
     news = Blog.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
-        'categories': categories,
     }
     return render(request, template_name='blof/index.html', context=context)
 
 
 def list_category(request, category_id):
     news = Blog.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, template_name='blof/category.html', context=context)
