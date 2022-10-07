48343b9e76b8932509fcb5705a1b6be3f859af00
Alex Malyshev <greeshanka@gmail.com>
2022-10-07 21:03:12 +0300

    changed data base from postgresql

--- blof_for_you/blof_for_you/settings.py
+++ blof_for_you/blof_for_you/settings.py
@@ -71,10 +71,21 @@ WSGI_APPLICATION = 'blof_for_you.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -115,7 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Указывает путь к
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blof_for_you/static'),
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
