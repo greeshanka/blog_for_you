7aaf0f056a04d76f7ab584005786df2ad721db07
Alex Malyshev <greeshanka@gmail.com>
2022-10-09 20:38:54 +0300

    new bootstrap component - accordion; change component List group; added apps - django-debug-toolbar

--- blof_for_you/blof_for_you/settings.py
+++ blof_for_you/blof_for_you/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blof.apps.BlofConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 ROOT_URLCONF = 'blof_for_you.urls'
@@ -134,3 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
