0408b6c33c389d5886a76c570e81102e057f9cab
Alex Malyshev <greeshanka@gmail.com>
2022-10-19 17:28:26 +0300

    created login.html, register.html; new path from tamplates; create func login and register

--- blof_for_you/blof/urls.py
+++ blof_for_you/blof/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('category/<int:category_id>/', CategoryListBlog.as_view(), name='list_category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add_news/', CreateNews.as_view(), name='add_news'),
+    path('news/register/', register, name='register'),
+    path('news/login/', login, name='login'),
 
 ]
--- blof_for_you/blof/views.py
+++ blof_for_you/blof/views.py
@@ -1,8 +1,38 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Blog, Category
 from .forms import BlogForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Gooooood!')
+            return redirect('login')
+        else:
+            messages.error(request, 'WTF?!')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'blof/register.html', context={'form': form})
+
+
+def login(request):
+    # if request.method == "POST":
+    #     form = UserLoginForm(data=request.POST)
+    #     if form.is_valid():
+    #         user = form.get_user()
+    #         login(request, user)
+    #         return redirect("home")
+    # else:
+    #     form = UserLoginForm()
+    return render(request, 'blof/login.html')
+
 
 
 class HomeBlog(ListView):
@@ -46,4 +76,3 @@ class ViewNews(DetailView):
 class CreateNews(CreateView):
     form_class = BlogForm
     template_name = 'blof/add_news.html'
-
