9f4377e7c5ba90b17fc9911ae933e922a3b4e603
Alex Malyshev <greeshanka@gmail.com>
2023-02-15 21:02:29 +0300

    Update README.md

--- blof_for_you/blof_for_you/settings.py
+++ blof_for_you/blof_for_you/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
